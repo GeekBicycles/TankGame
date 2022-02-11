e9b78c620832e833a98b59416819b282848a9fbd
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-02-11 19:29:38 +0300

    Сделал интерфейс IEnemyTank

---
+++ Assets/Interfaces/IEnemyTank.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemyTank : ITank
+{
+    /// <summary>
+    /// Максимальный разброс точности
+    /// </summary>
+    public float maxAccuracy { get; set; }
+
+    /// <summary>
+    /// Текущий разброс точности
+    /// </summary>
+    public float currentAccuracy { get; set; }
+
+    /// <summary>
+    /// Скорость изменения точности
+    /// </summary>
+    public float changeAccuracy { get; set; }
+
+}
