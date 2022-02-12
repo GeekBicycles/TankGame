50f1f690b5fbf1838e4268653048539410cbd243
olga-alekseeva <83349435+olga-alekseeva@users.noreply.github.com>
2022-02-12 23:54:12 +0300

    Добавила папку код
    В папке пока не разделяла на MVC папки, чтобы не запутаться пока файлов мало
    GameController запускает все объекты на сцене
    Reference объявляет объекты, т.к. проект еще маленький, то такой скрипт один, позже туда еще добавлю врага, м.б. бонусы и т.п.
    Ну и для развлечения кнопка рестарта и канвас на котором можно грузить дисплэй бонусов, жизни и т.п.

---
+++ Code/CharacterData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Tank_Game
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "CreateScriptablePbject/Properties/CharacterSettings", order = 3)]
+
+    public class CharacterData : ScriptableObject
+    {
+
+        [Range(0, 10)]
+        public float Speed;
+
+        [Range(0, 5)]
+        public float Health;
+
+        [Range(0, 10)]
+        public float Damage;
+
+
+
+
+    }
+}
