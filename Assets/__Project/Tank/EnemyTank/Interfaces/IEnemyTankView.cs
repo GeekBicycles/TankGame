9a7d379274390ffa33d9c0637b89dfb6ad2b50cd
MichaelRelov <Eighty5.Unity@gmail.com>
2022-02-17 14:11:00 +0200

    и еще что-то

--- Assets/__Project/Tank/EnemyTank/Interfaces/IEnemyTankView.cs
+++ Assets/__Project/Tank/EnemyTank/Interfaces/IEnemyTankView.cs
@@ -6,6 +6,11 @@ namespace Tank_Game
 {
     public interface IEnemyTankView
     {
+<<<<<<< Updated upstream:Assets/__Project/Tank/EnemyTank/Interfaces/IEnemyTankView.cs
+=======
+        public float Speed { get; set;  }
+        public int Damage { get; set; }
+>>>>>>> Stashed changes:Assets/__Project/Interfaces/IBullet.cs
         public Transform transform { get; set; }
         public Transform bulletSpawnTransform { get; set; }
     }
