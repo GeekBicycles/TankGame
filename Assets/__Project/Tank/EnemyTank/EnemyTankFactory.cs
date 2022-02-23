78ee7aec3e154f338a1729527068661bb4d99fcc
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-02-23 22:12:17 +0300

    Переделал фабрику на создание вражеских танков из прототипа

--- Assets/__Project/Tank/EnemyTank/EnemyTankFactory.cs
+++ Assets/__Project/Tank/EnemyTank/EnemyTankFactory.cs
@@ -5,13 +5,15 @@ namespace Tank_Game
     public class EnemyTankFactory : IEnemyTankFactory
     {
         private IPoolGameObject _poolGameObject;
+        private IEnemyTank enemyTankPrototype;
         public EnemyTankFactory()
         {
             GameObject prefab = Resources.Load<GameObject>(ResourcesPathes.ENEMY_TANK_PREFAB);
             _poolGameObject = new PoolGameObject(prefab, PrefabsNames.ENEMY_TANK, PrefabsNames.ENEMY_TANK_POOL);
+            enemyTankPrototype = null;
         }
 
-        public IEnemyTank GetEnemyTank(Vector3 position, Quaternion rotation)
+        private IEnemyTank GetNewEnemyTank(Vector3 position, Quaternion rotation)
         {
             EnemyTankModel enemyTankModel = Resources.Load<EnemyTankModel>(ResourcesPathes.ENEMY_TANK_MODEL);
 
@@ -22,6 +24,27 @@ namespace Tank_Game
 
             EnemyTank enemyTank = new EnemyTank(enemyTankModel, enemyTankView);
             enemyTankView.enemyTankBehaviour.enemyTank = enemyTank;
+
+            return enemyTank;
+        }
+
+        public IEnemyTank GetEnemyTank(Vector3 position, Quaternion rotation)
+        {
+            IEnemyTank enemyTank;
+
+            if (enemyTankPrototype == null)
+            {
+                enemyTank = GetNewEnemyTank(position, rotation);
+                enemyTankPrototype = (IEnemyTank)enemyTank.Clone();
+            }
+            else
+            {
+                enemyTank = (IEnemyTank)enemyTankPrototype.Clone();
+                GameObject gameObject = _poolGameObject.Pop();
+                gameObject.transform.position = position;
+                gameObject.transform.rotation = rotation;
+                enemyTank.view.AttachTransform(gameObject.transform, enemyTank);
+            }
 
             return enemyTank;
         }
